29ace0320388e3ad6975275e2f6988b01aaeacc7
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-06-04 08:28:22 -0300

    Adicionando arquivo de fibonacci calculado com dp

---
+++ fibonacci_dp.py
@@ -0,0 +1,22 @@
+def fibonacci(n):
+    FibArray = [0, 1]
+     
+    while len(FibArray) < n + 1: 
+        FibArray.append(0) 
+     
+    if n <= 1:
+       return n
+    else:
+       if FibArray[n - 1] ==  0:
+           FibArray[n - 1] = fibonacci(n - 1)
+     
+       if FibArray[n - 2] ==  0:
+           FibArray[n - 2] = fibonacci(n - 2)
+     
+       FibArray[n] = FibArray[n - 2] + FibArray[n - 1]
+    return FibArray[n]
+     
+print('Calculo de FIBONACCI com programação dinâmica')
+num1 = int(input('Entre com o número: '))
+print('O valor de fibonnaci de %d é: ' % num1)
+print(fibonacci(num1))
